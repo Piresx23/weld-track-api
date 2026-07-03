8cad6a77ebdd70cdc881577ba55713bf4fc6a2b6
Piresx23 <rodrigo.pires@education.mindera.com>
2026-07-03 16:34:19 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	pom.xml
    #	src/main/java/pt/weldtrack/weld/track_api/secutiry/SecurityInterceptor.java

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 DELETE FROM soldadores;
 
 INSERT INTO soldadores (soldador_name, password, papel)
-VALUES ('Joao', '$2a$10$iN.6gq1p1c2Vz5S6vO.2/.w/v1W5E1T1uU/7S6L1K1f/1z4h1t1s1', 'ADMIN');
+VALUES ('Joao', '$2a$10$tJ0p/5XqB5jJ5QyGz5H5UuD5XQ5Wk3Pj8Z4o4n4i/wW/yYjJp7.y2', 'ADMIN');
 
 INSERT INTO soldadores (soldador_name, password, papel)
 VALUES ('Carlos', '$2a$10$iN.6gq1p1c2Vz5S6vO.2/.w/v1W5E1T1uU/7S6L1K1f/1z4h1t1s1', 'USER');
